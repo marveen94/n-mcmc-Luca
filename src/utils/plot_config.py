2190a82826672466b096f41d710e783b48665f72
marveen94 <lucabrodoloni.94@gmail.com>
2023-11-03 15:11:29 +0100

    add plot config

---
+++ src/utils/plot_config.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.dpi"] = 200  # Risoluzione predefinita del grafico in DPI
+plt.rcParams["font.size"] = 18  # Dimensione predefinita dei caratteri
+plt.rcParams["lines.linewidth"] = 1.5  # Spessore predefinito delle linee
+plt.rcParams[
+    "savefig.format"
+] = "png"  # Formato predefinito di salvataggio (png, pdf, svg, ...)
+plt.rcParams["text.usetex"] = True  # Usa LateX
