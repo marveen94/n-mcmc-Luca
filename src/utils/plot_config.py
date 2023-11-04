4703e27dc6fb01d69b48a6cf32b06da77c65fd1a
marveen94 <lucabrodoloni.94@gmail.com>
2023-11-04 17:49:28 +0100

    add default facecolor

--- src/utils/plot_config.py
+++ src/utils/plot_config.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 plt.rcParams["figure.dpi"] = 200  # Risoluzione predefinita del grafico in DPI
+plt.rcParams["figure.facecolor"] = "w"
 plt.rcParams["font.size"] = 18  # Dimensione predefinita dei caratteri
 plt.rcParams["lines.linewidth"] = 1.5  # Spessore predefinito delle linee
 plt.rcParams[
